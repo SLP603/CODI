8092dbaf79ac185e757c14eb5d649b7f1947408e
CartWill <William.A.Carter@cuanschutz.edu>
2021-04-30 17:59:58 -0600

    - Added alias since table name may not match

--- Partner_Step_1/sql/ec_test.sql
+++ Partner_Step_1/sql/ec_test.sql
@@ -4,7 +4,7 @@ SELECT *
 INTO #ec_test
 FROM (
 	SELECT *
-	FROM @SCHEMA.@CENSUS_LOCATION
-	INNER JOIN #enc_counts ON enc_counts.patid = CENSUS_LOCATION.PERSON_ID
+	FROM @SCHEMA.@CENSUS_LOCATION cl
+	INNER JOIN #enc_counts ON enc_counts.patid = cl.PERSON_ID
 	WHERE loc_start <= CONVERT(DATETIME, '12-31-' + CAST(yr AS VARCHAR(4)))
 	) a;
