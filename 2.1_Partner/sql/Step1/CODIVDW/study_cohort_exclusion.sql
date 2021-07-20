d89e35e060680038651dc728dbbcde549a7bea60
CartWill <William.A.Carter@cuanschutz.edu>
2021-07-20 16:16:45 -0600

    Removed duplication case so age is just calculated then filtered at the end for cohort inclusion and cohort exclusion.

--- 2.1_Partner/sql/Step1/CODIVDW/study_cohort_exclusion.sql
+++ 2.1_Partner/sql/Step1/CODIVDW/study_cohort_exclusion.sql
@@ -1,31 +1,30 @@
 DROP TABLE IF EXISTS #study_cohort_exclusion;
-SELECT PERSON_ID AS patid
-	,birth_date
-	,CASE 
-		WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2017'), birth_date), '1/1/2017') < 0
-			THEN DATEDIFF(YEAR, birth_date, '1/1/2017') - 1
-		ELSE DATEDIFF(YEAR, birth_date, '1/1/2017')
-		END AS study_age_yrs
-	,CASE 
-		WHEN exc.patid IS NOT NULL
-			THEN 1
-		ELSE 0
-		END AS exclusion
+SELECT *
 INTO #study_cohort_exclusion
-FROM @SCHEMA.@DEMOGRAPHICS d
-LEFT JOIN (
-	SELECT DISTINCT @PERSON_ID_PATID
-	FROM @SCHEMA.@SESSION
-	WHERE session_date >= '1-Jun-2016'
-		AND session_date < '1-Jan-2017'
-		--AND programid IN (
-		--	SELECT programid
-		--	FROM #study_programs
-		--	)
-	) exc ON exc.patid = d.PERSON_ID
-WHERE CASE 
-		WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2017'), birth_date), '1/1/2017') < 0
-			THEN DATEDIFF(YEAR, birth_date, '1/1/2017') - 1
-		ELSE DATEDIFF(YEAR, birth_date, '1/1/2017')
-		END BETWEEN 2
+FROM (
+	SELECT PERSON_ID AS patid
+		,birth_date
+		,CASE 
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2017'), birth_date), '1/1/2017') < 0
+				THEN DATEDIFF(YEAR, birth_date, '1/1/2017') - 1
+			ELSE DATEDIFF(YEAR, birth_date, '1/1/2017')
+			END AS study_age_yrs
+		,CASE 
+			WHEN exc.patid IS NOT NULL
+				THEN 1
+			ELSE 0
+			END AS exclusion
+	FROM @SCHEMA.@DEMOGRAPHICS d
+	LEFT JOIN (
+		SELECT DISTINCT @PERSON_ID_PATID patid
+		FROM @SCHEMA.@SESSION
+		WHERE session_date >= '1-Jun-2016'
+			AND session_date < '1-Jan-2017'
+			--AND programid IN (
+			--	SELECT programid
+			--	FROM #study_programs
+			--	)
+		) exc ON exc.patid = d.PERSON_ID
+	) a
+WHERE study_age_yrs BETWEEN 2
 		AND 19;
--- 2.1_Partner/sql/Step1/CODIVDW/study_cohort_inclusion.sql
+++ 2.1_Partner/sql/Step1/CODIVDW/study_cohort_inclusion.sql
@@ -1,26 +1,25 @@
 DROP TABLE IF EXISTS #study_cohort_inclusion;
-SELECT PERSON_ID AS patid
-	,birth_date
-	,CASE 
-		WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2017'), birth_date), '1/1/2017') < 0
-			THEN DATEDIFF(YEAR, birth_date, '1/1/2017') - 1
-		ELSE DATEDIFF(YEAR, birth_date, '1/1/2017')
-		END AS study_age_yrs
-	,CASE 
-		WHEN inc.patid IS NOT NULL
-			THEN 1
-		ELSE 0
-		END AS inclusion
+SELECT *
 INTO #study_cohort_inclusion
-FROM @SCHEMA.@DEMOGRAPHICS d
-LEFT JOIN (
-	SELECT DISTINCT @PERSON_ID_PATID COLLATE
-	FROM @SCHEMA.@SESSION
-	WHERE DATEPART(YEAR, session_date) = 2017
-	) inc ON inc.patid = d.PERSON_ID
-WHERE CASE 
-		WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2017'), birth_date), '1/1/2017') < 0
-			THEN DATEDIFF(YEAR, birth_date, '1/1/2017') - 1
-		ELSE DATEDIFF(YEAR, birth_date, '1/1/2017')
-		END BETWEEN 2
+FROM (
+	SELECT PERSON_ID AS patid
+		,birth_date
+		,CASE 
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2017'), birth_date), '1/1/2017') < 0
+				THEN DATEDIFF(YEAR, birth_date, '1/1/2017') - 1
+			ELSE DATEDIFF(YEAR, birth_date, '1/1/2017')
+			END AS study_age_yrs
+		,CASE 
+			WHEN inc.patid IS NOT NULL
+				THEN 1
+			ELSE 0
+			END AS inclusion
+	FROM @SCHEMA.@DEMOGRAPHICS d
+	LEFT JOIN (
+		SELECT DISTINCT @PERSON_ID_PATID patid
+		FROM @SCHEMA.@SESSION
+		WHERE DATEPART(YEAR, session_date) = 2017
+		) inc ON inc.patid = d.PERSON_ID
+	) a
+WHERE study_age_yrs BETWEEN 2
 		AND 19;
