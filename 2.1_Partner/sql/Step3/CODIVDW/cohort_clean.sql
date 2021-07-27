0621d7649f786e40eaa44f2d9b3d0055c1388092
CartWill <William.A.Carter@cuanschutz.edu>
2021-07-27 11:56:12 -0600

    - Rewrote cohort_clean.sql in order to reduce execution time.

--- 2.1_Partner/sql/Step3/CODIVDW/cohort_clean.sql
+++ 2.1_Partner/sql/Step3/CODIVDW/cohort_clean.sql
@@ -1,9 +1,11 @@
-DELETE #cohort
-WHERE patid IN (
-		SELECT patid
-		FROM #cohort
-		WHERE patid NOT IN (
-				SELECT patid
-				FROM #anchor_date
-				)
-		);
+WITH cte_cohort_clean as (
+SELECT distinct a.patid
+		FROM #cohort a
+		LEFT JOIN #anchor_date b 
+			on b.patid = a.patid
+		WHERE b.patid is null
+)
+DELETE d
+FROM #cohort d
+JOIN cte_cohort_clean c 
+	on c.patid = d.patid;
