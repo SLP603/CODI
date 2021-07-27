1b8817879d50eb83298c0871e45cf33a483112cf
CartWill <William.A.Carter@cuanschutz.edu>
2021-07-27 13:59:19 -0600

    - Reduced the number of inserts statements in comorb_codes.sql to improve efficiency  - Pulled LIKE join into a separate query in coconditions.sql to try and reduce operations when joining comorb codes.

--- 2.1_Partner/sql/Step3/CODIVDW/coconditions.sql
+++ 2.1_Partner/sql/Step3/CODIVDW/coconditions.sql
@@ -3,6 +3,13 @@
 	because referencedComponentId are all snomed codes and diagnoses.dx are (mostly) ICD codes.
 */
 DROP TABLE IF EXISTS #coconditions;
+WITH cte_comorb_codes as (
+	SELECT * 
+	FROM #snomed2icd s
+	JOIN #comorb_codes c 
+		ON UPPER(RTRIM(LTRIM(c.code))) LIKE UPPER(RTRIM(LTRIM(s.mapTarget))) + '%'
+)
+
 SELECT d.DIAGNOSES_ID AS diagnosisid
 	,d.PERSON_ID AS patid
 	,d.ADATE AS admit_date
@@ -13,8 +20,7 @@ SELECT d.DIAGNOSES_ID AS diagnosisid
 	,condition
 INTO #coconditions
 FROM @SCHEMA.@DIAGNOSES d
-JOIN #snomed2icd s ON d.dx = s.mapTarget
-JOIN #comorb_codes c ON UPPER(RTRIM(LTRIM(c.code))) LIKE UPPER(RTRIM(LTRIM(s.mapTarget))) + '%'
+JOIN cte_comorb_codes s ON d.dx = s.mapTarget
 JOIN #anchor_date a ON a.patid = d.PERSON_ID
 WHERE ADATE < a.measure_date
 	AND ADATE >= DATEADD(MONTH, -8, a.measure_date);
