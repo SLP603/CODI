1fb8be519b7a45b68fed373945bc7383a93e7824
CartWill <William.A.Carter@cuanschutz.edu>
2021-07-27 12:36:30 -0600

    Copied changes made in step 3 to step 6

--- 2.1_Partner/sql/Step3/CODIVDW/cohort_clean.sql
+++ 2.1_Partner/sql/Step3/CODIVDW/cohort_clean.sql
@@ -1,11 +1,11 @@
 WITH cte_cohort_clean as (
-SELECT distinct a.patid
-		FROM #cohort a
-		LEFT JOIN #anchor_date b 
-			on b.patid = a.patid
-		WHERE b.patid is null
+	SELECT distinct a.patid
+	FROM #cohort a
+	LEFT JOIN #anchor_date b 
+		ON b.patid = a.patid
+	WHERE b.patid is null
 )
 DELETE d
 FROM #cohort d
 JOIN cte_cohort_clean c 
-	on c.patid = d.patid;
+	ON c.patid = d.patid;
--- 2.1_Partner/sql/Step6/CODIVDW/delete_from_cohort.sql
+++ 2.1_Partner/sql/Step6/CODIVDW/delete_from_cohort.sql
@@ -1,9 +1,11 @@
-DELETE #cohort
-WHERE patid IN (
-		SELECT patid
-		FROM #cohort
-		WHERE patid NOT IN (
-				SELECT patid
-				FROM #anchor_date
-				)
-		);
+WITH cte_cohort_clean as (
+	SELECT distinct a.patid
+	FROM #cohort a
+	LEFT JOIN #anchor_date b 
+		ON b.patid = a.patid
+	WHERE b.patid is null
+)
+DELETE d
+FROM #cohort d
+JOIN cte_cohort_clean c 
+	ON c.patid = d.patid;
