6d9443522a2e2c1573930501603094cdc7ee7561
CartWill <William.A.Carter@cuanschutz.edu>
2021-08-16 17:51:29 -0600

    - Updated profile variable name because of redundancy.  - Removed select from inside main query of insurance.sql because it can have an negative impact on performance.

--- 2.1_DCC/.Rprofile
+++ 2.1_DCC/.Rprofile
@@ -1,6 +1,6 @@
-rVersion <- version[['major']]
-cat(paste0("R version ",rVersion," detected\n"))
-Sys.setenv(RENV_PROFILE = paste0("R", rVersion, sep=''))
+rMjrVersion <- version[['major']]
+cat(paste0("R version ",rMjrVersion, " detected\n"))
+Sys.setenv(RENV_PROFILE = paste0("R", rMjrVersion, sep=''))
 options(pkgType="win.binary")
 options(install.packages.check.source = "no")
 options(install.packages.compile.from.source = "never")
--- 2.1_Partner/sql/Step3/CODIVDW/insurance.sql
+++ 2.1_Partner/sql/Step3/CODIVDW/insurance.sql
@@ -3,11 +3,11 @@ SELECT
 	e.ENC_ID, 
 	e.PERSON_ID,
 	BENEFIT_CAT, 
-	(SELECT CASE 
+	CASE 
 		WHEN BENEFIT_CAT IN ('CC', 'CP', 'MC', 'MD') THEN 'Public (non-military)'
 		WHEN BENEFIT_CAT IN ('CO') THEN 'Private'
 		ELSE 'Other or unknown' -- OG, NC, OT, UN, WC, NI
-	END) as insurance_type
+	END as insurance_type
 INTO #insurance	
 FROM @SCHEMA.@ENCOUNTERS e
 	LEFT JOIN (SELECT * FROM @BENEFIT_SCHEMA.@BENEFIT WHERE BENEFIT_TYPE = 'PR') BEN ON e.ENC_ID = BEN.ENC_ID
