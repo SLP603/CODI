a506928fb165516003dbeab51705a034176c259e
Samantha Lange Pierce <128009098+SLP603@users.noreply.github.com>
2023-03-28 16:16:23 -0400

    Update cohort_demographic_age_filter.sql

--- 1.4_Partner/sql/Step3/CODIVDW/cohort_demographic_age_filter.sql
+++ 1.4_Partner/sql/Step3/CODIVDW/cohort_demographic_age_filter.sql
@@ -14,11 +14,17 @@ FROM (
   	loc_start --,
   	--study_age_yrs_2017,
   	--study_age_yrs_2018,
-  	--study_age_yrs_2019
+  	--study_age_yrs_2019,
+    --study_age_yrs_2020,
+    --study_age_yrs_2021,
+    --study_age_yrs_2022
   FROM 
   	#cohort_demographic_age 
   WHERE 
   	(yr = 2017 AND study_age_yrs_2017 BETWEEN 2 AND 19) OR
   	(yr = 2018 AND study_age_yrs_2018 BETWEEN 2 AND 19) OR
-  	(yr = 2019 AND study_age_yrs_2019 BETWEEN 2 AND 19)
+  	(yr = 2019 AND study_age_yrs_2019 BETWEEN 2 AND 19) OR
+    (yr = 2020 AND study_age_yrs_2020 BETWEEN 2 AND 19) OR
+    (yr = 2021 AND study_age_yrs_2021 BETWEEN 2 AND 19) OR
+    (yr = 2022 AND study_age_yrs_2022 BETWEEN 2 AND 19)
 ) a;
