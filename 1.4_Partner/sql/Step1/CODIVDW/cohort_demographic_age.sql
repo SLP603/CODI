655ea20a7977f90288e52ad21b3efa9283e3bc3b
Samantha Lange Pierce <128009098+SLP603@users.noreply.github.com>
2023-03-28 16:14:15 -0400

    Update cohort_demographic_age.sql

--- 1.4_Partner/sql/Step1/CODIVDW/cohort_demographic_age.sql
+++ 1.4_Partner/sql/Step1/CODIVDW/cohort_demographic_age.sql
@@ -18,5 +18,20 @@ FROM (
 				THEN DATEDIFF(YEAR, birth_date, '1/1/2019') - 1
 			ELSE DATEDIFF(YEAR, birth_date, '1/1/2019')
 			END AS study_age_yrs_2019
+		,CASE 
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2020'), birth_date), '1/1/2020') < 0
+				THEN DATEDIFF(YEAR, birth_date, '1/1/2020') - 1
+			ELSE DATEDIFF(YEAR, birth_date, '1/1/2020')
+			END AS study_age_yrs_2020
+		,CASE 
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2021'), birth_date), '1/1/2021') < 0
+				THEN DATEDIFF(YEAR, birth_date, '1/1/2021') - 1
+			ELSE DATEDIFF(YEAR, birth_date, '1/1/2021')
+			END AS study_age_yrs_2021
+		,CASE 
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2022'), birth_date), '1/1/2022') < 0
+				THEN DATEDIFF(YEAR, birth_date, '1/1/2022') - 1
+			ELSE DATEDIFF(YEAR, birth_date, '1/1/2022')
+			END AS study_age_yrs_2022
 	FROM #cohort_demographic
 ) a;
